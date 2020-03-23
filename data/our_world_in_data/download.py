d7e9c2deca5af9ea3f72e6808cb197a80167f12f
AlexDelitzas <alexanderdel9@gmail.com>
2020-03-23 02:54:43 +0200

    Cosmetic changes

--- data/our_world_in_data/download.py
+++ data/our_world_in_data/download.py
@@ -7,11 +7,10 @@ DOWNLOADS_DIR = './'
 print('Downloading data ...')
 
 for url in open('urls.txt'):
-    print(url)
+
     name = url.rsplit('/', 1)[-1].strip()
-    print(name)
     filename = os.path.join(DOWNLOADS_DIR, name)
-    print(filename)
+
     try:
         urllib.request.urlretrieve(url, filename)
     except Exception as inst:
