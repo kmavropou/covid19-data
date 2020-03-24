483b2caaf4312cea4a9d7847c02db6df576aa57f
AlexDelitzas <alexanderdel9@gmail.com>
2020-03-24 20:20:09 +0200

    Add data conversion to JSON file

---
+++ process/extract_json.py
@@ -0,0 +1,37 @@
+# this script converts the data from Johns Hopkins CSSE into a JSON file
+
+import pandas as pd
+import json
+
+def export_to_json(data, filename):
+    json_output = '{\n'
+    for name, group in data.groupby(["Country/Region"]):
+        json_output += ('\n\t"' + name + '": [')
+        for index, row in group.iterrows():
+            json_output += '\n\t\t{\n\t\t\t'
+
+            json_output += ('"date": "' + str(index[1])[:-9] + '",')
+            json_output += ('\n\t\t\t"confirmed": ' + str(int(row['Confirmed'])) + ',')
+            json_output += ('\n\t\t\t"recovered": ' + str(int(row['Recovered'])) + ',')
+            json_output += ('\n\t\t\t"deaths": ' + str(int(row['Deaths'])))
+            json_output += '\n\t\t},'
+
+        json_output = json_output[:-1]
+        json_output += '\n\t],'
+    json_output = json_output[:-1]
+    json_output += '\n}'
+
+
+    with open(filename, 'w') as outfile:
+        outfile.write(json_output)
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('../output_data/covid19_clean_complete.csv')
+
+    data = data.drop(columns=["Province/State", "Lat", "Long"])
+
+    data['Date'] = pd.to_datetime(data['Date'])
+    data = data.groupby(["Country/Region", "Date"])["Confirmed", "Deaths", "Recovered"].sum()
+
+    export_to_json(data, '../output_data/covid19_timeseries.json')
